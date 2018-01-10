a9a8735b56685b543443bb6835a1e7d32296e8fa
Dmitry Pavliv <simoco@yandex.ru>
2018-01-10 12:45:53 +0300

    Additional unit test

--- TimingScope/TimingScope.Tests/Tests.cs
+++ TimingScope/TimingScope.Tests/Tests.cs
@@ -87,5 +87,63 @@ namespace TimingScope.Tests
                 }
             }
         }
+
+        [TestMethod]
+        public void TestFromMultipleMethods()
+        {
+            Parallel.Invoke
+            (
+                () => RootMethod(Guid.NewGuid(), "Call_1"),
+                () => RootMethod(Guid.NewGuid(), "Call_2"),
+                () => RootMethod(Guid.NewGuid(), "Call_3")
+            );
+        }
+        
+        private void RootMethod(Guid id, string anotherParam)
+        {
+            //Arrange
+            using (var scope = TimingScope.Create())
+            {
+                //Set some context properties. May be helpful for future logging
+                scope.SetProperty("ObjectId", id.ToString())
+                    .SetProperty("AnotherParam", anotherParam);
+
+                var startTime = DateTimeOffset.Now;
+
+                int n = 100;
+
+                //Act
+                Parallel.For(0, n, (i) =>
+                {
+                    InnerMethod(i);
+                });
+
+                TimingScope.Current.Log("RootMethod", startTime, DateTimeOffset.Now);
+
+                //Assert
+                var logEntries = TimingScope.Current.GetLogEntries();
+                var properties = TimingScope.Current.GetProperties();
+
+                for (int i = 0; i < n; i++)
+                {
+                    Assert.AreEqual(1, logEntries.Count(x => x.Name == $"InnerMethod({i})"));
+                }
+                Assert.AreEqual(1, logEntries.Count(x => x.Name == "RootMethod"));
+
+                Assert.AreEqual(2, properties.Count);
+                Assert.AreEqual(id.ToString(), properties["ObjectId"]);
+                Assert.AreEqual(anotherParam, properties["AnotherParam"]);
+            }
+        }
+
+        private void InnerMethod(int i)
+        {
+            var startTime = DateTimeOffset.Now;
+
+            //Some Work
+            Thread.Sleep(100);
+
+            TimingScope.Current.Log($"InnerMethod({i})", startTime, DateTimeOffset.Now);
+        }
     }
 }
